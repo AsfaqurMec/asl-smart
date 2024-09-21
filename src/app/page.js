13fe9716029c077a4b3add07a52b3b71167ae1fa
AsfaqurMec <hamimhamim044@gmail.com>
2024-09-22 01:32:56 +0600

    created About section

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Banner from "../../Components/Banner";
+import About from "../../Components/About";
 
 export default function Home() {
   return (
     <>
     <Banner></Banner>
+    <About></About>
     </>
   );
 }
