37263c85b9fd200e8796d38a4f9ca79ebe15e8f1
AsfaqurMec <hamimhamim044@gmail.com>
2024-09-22 16:13:15 +0600

    updating

--- src/app/Components/Banner.jsx
+++ src/app/Components/Banner.jsx
@@ -11,65 +11,65 @@ import img1 from '../../../public/Screenshot 2024-08-19 223339.png'
 import img2 from '../../../public/Screenshot 2024-08-19 223353.png'
 
 const Banner = () => {
-  
+
   useEffect(() => {
     AOS.init({});
-}, []); 
-  
-    return (
-        <>
-        <div>
-       <div className="md:hero bg-[#07332f] pb-20 lg:pb-0 min-h-screen pt-20">
-  <div className="flex md:hero-content flex-col lg:flex-row-reverse px-3 gap-20 lg:gap-0">
-    <Image data-aos="fade-up"  data-aos-duration="2000" src={banner} alt='banner' className="w-[90%] mx-auto md:max-w-xl"></Image>
-    
-    <div className='px-10 md:px-5'>
-        <p data-aos="zoom-out-down" data-aos-duration="1000" className='uppercase tracking-widest text-xl text-white font-thin'>we tack care of your health</p>
-      <h1 data-aos="fade-down" data-aos-duration="1000" data-aos-delay="100" className="text-5xl font-bold text-[#f7a582] mt-3">We Are Providing</h1>
-      <h1 data-aos="fade-down" data-aos-duration="1000" data-aos-delay="300" className="text-5xl font-bold text-[#f7a582] my-2">Best & Affordable</h1>
-      <h1 data-aos="fade-down" data-aos-duration="1000" data-aos-delay="500" className="text-5xl font-bold text-[#f7a582]">Health Care.</h1>
-      <p data-aos="fade-up"  data-aos-duration="1000" data-aos-delay="500" className="py-6 text-xl text-slate-400">
-      our is to deliver the highest quality healthcare services. We believe that everyone deserves access to excellent medical care without compromising on quality.
-      </p>
-      <div data-aos="fade-up"  data-aos-duration="1000" data-aos-delay="600" className='flex flex-col md:flex-row gap-5'>
-      <button className="btn bg-transparent font-semibold text-xl rounded-xl border-2 text-[#f7a582] btn-outline hover:bg-[#f7a582]">Read More</button>
-      <button className="btn bg-transparent font-semibold text-xl text-[#ffffffd7] border-none hover:bg-[#f7a582]">Watch video</button>
+  }, []);
+
+  return (
+    <>
+      <div>
+        <div className="md:hero bg-[#07332f] pb-20 lg:pb-0 min-h-screen pt-20">
+          <div className="flex md:hero-content flex-col lg:flex-row-reverse px-3 gap-20 lg:gap-0">
+            <Image data-aos="fade-up" data-aos-duration="2000" src={banner} alt='banner' className="w-[90%] mx-auto md:max-w-xl"></Image>
+
+            <div className='px-10 md:px-5'>
+              <p data-aos="zoom-out-down" data-aos-duration="1000" className='uppercase tracking-widest text-xl text-white font-thin'>we tack care of your health</p>
+              <h1 data-aos="fade-down" data-aos-duration="1000" data-aos-delay="100" className="text-5xl font-bold text-[#f7a582] mt-3">We Are Providing</h1>
+              <h1 data-aos="fade-down" data-aos-duration="1000" data-aos-delay="300" className="text-5xl font-bold text-[#f7a582] my-2">Best & Affordable</h1>
+              <h1 data-aos="fade-down" data-aos-duration="1000" data-aos-delay="500" className="text-5xl font-bold text-[#f7a582]">Health Care.</h1>
+              <p data-aos="fade-up" data-aos-duration="1000" data-aos-delay="500" className="py-6 text-xl text-slate-400">
+                our is to deliver the highest quality healthcare services. We believe that everyone deserves access to excellent medical care without compromising on quality.
+              </p>
+              <div data-aos="fade-up" data-aos-duration="1000" data-aos-delay="600" className='flex flex-col md:flex-row gap-5'>
+                <button className="btn bg-transparent font-semibold text-xl rounded-xl border-2 text-[#f7a582] btn-outline hover:bg-[#f7a582]">Read More</button>
+                <button className="btn bg-transparent font-semibold text-xl text-[#ffffffd7] border-none hover:bg-[#f7a582]">Watch video</button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-</div>     
+
+      {/* Lower Banner section */}
+
+      <section data-aos="fade-up" data-aos-duration="2000" className="my-20">
+
+        <div className="w-[80%] mx-auto flex flex-col lg:flex-row">
+          <div className="h-56 w-full lg:w-1/3 bg-[#f7a582]">
+            <div className="h-[78.6%] flex flex-col justify-center font-semibold text-white text-3xl">
+              <h1 className="pl-5 pt-5">Don't Hesitate To</h1>
+              <h1 className="pl-5 ">Contact us</h1>
+            </div>
+            <button className="btn rounded-none w-full bg-black border-none text-white">Make Appointment</button>
+          </div>
+          <div className="h-[580px] md:h-[490px] lg:h-56 w-full lg:w-2/3 flex flex-col lg:flex-row bg-[#07332f] p-3">
+            <div className="w-full lg:w-1/2 p-8 gap-3 flex flex-col justify-center font-semibold text-white text-xl border-b-2 lg:border-b-0 border-r-0 lg:border-r-2">
+              <Image src={img1} alt="logo"></Image>
+              <h1>Need family health</h1>
+              <p className="text-base text-slate-400">we understand the importance of family health overall well-being.</p>
+            </div>
+            <div className="w-full lg:w-1/2 p-8 f flex gap-3 flex-col justify-center font-semibold text-white text-xl">
+              <Image src={img2} alt="logo"></Image>
+              <h1>24 hours service</h1>
+              <p className="text-base text-slate-400">we take pride in offering 24-hour medical services to ensure that you.</p>
+            </div>
+          </div>
         </div>
 
-        {/* Lower Banner section */}
 
-        <section data-aos="fade-up"  data-aos-duration="2000" className="my-20">
-
-<div className="w-[80%] mx-auto flex flex-col lg:flex-row">
-    <div className="h-56 w-full lg:w-1/3 bg-[#f7a582]">
-    <div className="h-[78.6%] flex flex-col justify-center font-semibold text-white text-3xl">
-       <h1 className="pl-5 pt-5">Don't Hesitate To</h1>
-       <h1 className="pl-5 ">Contact us</h1>
-       </div>
-       <button className="btn rounded-none w-full bg-black border-none text-white">Make Appointment</button>
-    </div>
-    <div className="h-[580px] md:h-[490px] lg:h-56 w-full lg:w-2/3 flex flex-col lg:flex-row bg-[#07332f] p-3">
-       <div className="w-full lg:w-1/2 p-8 gap-3 flex flex-col justify-center font-semibold text-white text-xl border-b-2 lg:border-b-0 border-r-0 lg:border-r-2">
-          <Image src={img1} alt="logo"></Image>
-          <h1>Need family health</h1>
-          <p className="text-base text-slate-400">we understand the importance of family health overall well-being.</p>
-       </div>
-       <div className="w-full lg:w-1/2 p-8 f flex gap-3 flex-col justify-center font-semibold text-white text-xl">
-       <Image src={img2} alt="logo"></Image>
-          <h1>24 hours service</h1>
-          <p className="text-base text-slate-400">we take pride in offering 24-hour medical services to ensure that you.</p>
-       </div>
-    </div>
-</div>
-
-
-</section>
-</>
-    );
+      </section>
+    </>
+  );
 };
 
 export default Banner;
--- src/app/Components/Services.jsx
+++ src/app/Components/Services.jsx
@@ -10,94 +10,88 @@ import img7 from '../../../public/Screenshot 2024-08-19 223503.png'
 import img8 from '../../../public/Screenshot 2024-08-19 223518.png'
 
 const Services = () => {
-    return (
-        <>
+   return (
+      <>
+         <section className=" bg-[#fff7f3] pb-20">
+            <div className="w-[80%] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
 
-        {/* section 3 */}
+               <div className="h-72 w-full ">
+                  <div className="h-full flex flex-col justify-center gap-3  font-semibold text-black text-5xl py-10">
+                     <p data-aos="zoom-in-down" data-aos-duration="1000" className='uppercase tracking-widest text-xl text-[#f29269] font-medium'>medical services</p>
+                     <h1 data-aos="zoom-in-up" data-aos-duration="1000">We're Providing</h1>
+                     <h1 data-aos="zoom-in-up" data-aos-duration="1000">Best Services.</h1>
+                  </div>
 
+               </div>
 
-   <section className=" bg-[#fff7f3] pb-20">
-     <div className="w-[80%] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-       
-     <div className="h-72 w-full ">
-                <div className="h-full flex flex-col justify-center gap-3  font-semibold text-black text-5xl py-10">
-                <p data-aos="zoom-in-down" data-aos-duration="1000" className='uppercase tracking-widest text-xl text-[#f29269] font-medium'>medical services</p>
-                      <h1 data-aos="zoom-in-up" data-aos-duration="1000">We're Providing</h1>
-                      <h1 data-aos="zoom-in-up" data-aos-duration="1000">Best Services.</h1>
-                   </div>
-                   
-                </div>
+               <div data-aos="zoom-in" data-aos-duration="1000" data-aos-delay="100" className="h-72 w-full border-[1px] border-black">
+                  <div className="h-[83.1%] flex flex-col justify-center gap-3  font-semibold text-black text-3xl p-10">
+                     <Image src={img3} alt="logo"></Image>
+                     <h1>eye care</h1>
+                     <p className="text-base text-slate-400">we understand the importance of clear vision and its impact on your.</p>
+                  </div>
+                  <button className="btn rounded-none w-full bg-[#f7a582] border-none text-white">Read More</button>
+               </div>
 
-                <div data-aos="zoom-in" data-aos-duration="1000" data-aos-delay="100" className="h-72 w-full border-[1px] border-black">
-                <div className="h-[83.1%] flex flex-col justify-center gap-3  font-semibold text-black text-3xl p-10">
-                <Image src={img3} alt="logo"></Image>
-                      <h1>eye care</h1>
-                      <p className="text-base text-slate-400">we understand the importance of clear vision and its impact on your.</p>
-                   </div>
-                   <button className="btn rounded-none w-full bg-[#f7a582] border-none text-white">Read More</button>
-                </div>
+               <div data-aos="zoom-in" data-aos-duration="1000" data-aos-delay="200" className="h-72 w-full border-[1px] border-black">
+                  <div className="h-[83.2%] flex flex-col justify-center gap-3  font-semibold text-black text-3xl p-10">
+                     <Image src={img4} alt="logo"></Image>
+                     <h1>medical checkup</h1>
+                     <p className="text-base text-slate-400">During your medical checkup, our skilled healthcare professionals.</p>
+                  </div>
+                  <button className="btn rounded-none w-full bg-[#f7a582] border-none text-white">Read More</button>
+               </div>
 
-                <div data-aos="zoom-in" data-aos-duration="1000" data-aos-delay="200" className="h-72 w-full border-[1px] border-black">
-                <div className="h-[83.2%] flex flex-col justify-center gap-3  font-semibold text-black text-3xl p-10">
-                <Image src={img4} alt="logo"></Image>
-                      <h1>medical checkup</h1>
-                      <p className="text-base text-slate-400">During your medical checkup, our skilled healthcare professionals.</p>
-                   </div>
-                   <button className="btn rounded-none w-full bg-[#f7a582] border-none text-white">Read More</button>
-                </div>
+               <div data-aos="zoom-in" data-aos-duration="1000" data-aos-delay="100" className="h-72 w-full border-[1px] border-black">
+                  <div className="h-[83.2%] flex flex-col justify-center gap-3  font-semibold text-black text-3xl p-10">
+                     <Image src={img5} alt="logo"></Image>
+                     <h1>dental care</h1>
+                     <p className="text-base text-slate-400">we are passionate about providing top-notch dental care to help you.</p>
+                  </div>
+                  <button className="btn rounded-none w-full bg-[#f7a582] border-none text-white">Read More</button>
+               </div>
 
-                <div data-aos="zoom-in" data-aos-duration="1000" data-aos-delay="100" className="h-72 w-full border-[1px] border-black">
-                <div className="h-[83.2%] flex flex-col justify-center gap-3  font-semibold text-black text-3xl p-10">
-                <Image src={img5} alt="logo"></Image>
-                      <h1>dental care</h1>
-                      <p className="text-base text-slate-400">we are passionate about providing top-notch dental care to help you.</p>
-                   </div>
-                   <button className="btn rounded-none w-full bg-[#f7a582] border-none text-white">Read More</button>
-                </div>
+               <div data-aos="zoom-in" data-aos-duration="1000" data-aos-delay="200" className="h-72 w-full border-[1px] border-black">
+                  <div className="h-[83.2%] flex flex-col justify-center gap-3  font-semibold text-black text-3xl p-10">
+                     <Image src={img6} alt="logo"></Image>
+                     <h1>laboratory service</h1>
+                     <p className="text-base text-slate-400">we understand the critical role that accurate diagnostics play in guiding.</p>
+                  </div>
+                  <button className="btn rounded-none w-full bg-[#f7a582] border-none text-white">Read More</button>
+               </div>
 
-                <div data-aos="zoom-in" data-aos-duration="1000" data-aos-delay="200" className="h-72 w-full border-[1px] border-black">
-                <div className="h-[83.2%] flex flex-col justify-center gap-3  font-semibold text-black text-3xl p-10">
-                <Image src={img6} alt="logo"></Image>
-                      <h1>laboratory service</h1>
-                      <p className="text-base text-slate-400">we understand the critical role that accurate diagnostics play in guiding.</p>
-                   </div>
-                   <button className="btn rounded-none w-full bg-[#f7a582] border-none text-white">Read More</button>
-                </div>
-
-                <div data-aos="zoom-in" data-aos-duration="1000" data-aos-delay="300" className="h-72 w-full border-[1px] border-black">
-                <div className="h-[83.2%] flex flex-col justify-center gap-3  font-semibold text-black text-3xl p-10">
-                <Image src={img7} alt="logo"></Image>
-                      <h1>Patient-centered</h1>
-                      <p className="text-base text-slate-400">hospitals, or clinics with positive reviews and ratings from patients.</p>
-                   </div>
-                   <button className="btn rounded-none w-full bg-[#f7a582] border-none text-white">Read More</button>
-                </div>
-     </div>
-
-   </section>
-
-
-    {/* section 4 */}
-
-
-   <section className="bg-[#07332f] w-full ">
-
-     <div className="flex flex-col gap-10 justify-evenly items-center lg:flex-row py-14">
-        <div className="flex flex-col justify-center lg:flex-row items-center gap-8 text-white">
-            <Image src={img8} alt="o"></Image>
-            <div className="text-center lg:text-left">
-                <h1 data-aos="zoom-in-up" data-aos-duration="1000" className="text-4xl mb-5">Open For Appointments</h1>
-                <p data-aos="zoom-in-down" data-aos-duration="1000">we are delighted to announce that our doors are open, and we are now accepting <br /> appointments to serve you better.</p>
+               <div data-aos="zoom-in" data-aos-duration="1000" data-aos-delay="300" className="h-72 w-full border-[1px] border-black">
+                  <div className="h-[83.2%] flex flex-col justify-center gap-3  font-semibold text-black text-3xl p-10">
+                     <Image src={img7} alt="logo"></Image>
+                     <h1>Patient-centered</h1>
+                     <p className="text-base text-slate-400">hospitals, or clinics with positive reviews and ratings from patients.</p>
+                  </div>
+                  <button className="btn rounded-none w-full bg-[#f7a582] border-none text-white">Read More</button>
+               </div>
             </div>
-            
-        </div>
-        <button data-aos="zoom-in-down" data-aos-duration="1000" className="btn bg-transparent font-semibold text-xl rounded-3xl border-2 text-[#f7a582] btn-outline hover:bg-left">Book Appointment</button>
-     </div>
 
-   </section>
+         </section>
 
-        </>
-    );
+
+
+         <section className="bg-[#07332f] w-full ">
+
+            <div className="flex flex-col gap-10 justify-evenly items-center lg:flex-row py-14">
+               <div className="flex flex-col justify-center lg:flex-row items-center gap-8 text-white">
+                  <Image src={img8} alt="o"></Image>
+                  <div className="text-center lg:text-left">
+                     <h1 data-aos="zoom-in-up" data-aos-duration="1000" className="text-4xl mb-5">Open For Appointments</h1>
+                     <p data-aos="zoom-in-down" data-aos-duration="1000">we are delighted to announce that our doors are open, and we are now accepting <br /> appointments to serve you better.</p>
+                  </div>
+
+               </div>
+               <button data-aos="zoom-in-down" data-aos-duration="1000" className="btn bg-transparent font-semibold text-xl rounded-3xl border-2 text-[#f7a582] btn-outline hover:bg-left">Book Appointment</button>
+            </div>
+
+         </section>
+
+      </>
+   );
 };
 
 export default Services;
